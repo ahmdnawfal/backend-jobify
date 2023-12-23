dffd976cfb8f30351a024d384ebe9ed8c966a6c3
Ahmad Nawfal <ahmadnawfal2003@gmail.com>
2023-12-24 01:41:05 +0700

    fix: server

--- server.js
+++ server.js
@@ -65,7 +65,7 @@ const port = process.env.PORT || 5100;
 
 try {
   await mongoose.connect(process.env.MONGO_URL);
-  mongoose.set('strictQuery', false);
+  await mongoose.set('strictQuery', false);
   app.listen(port, () => {
     console.log('server running');
   });
