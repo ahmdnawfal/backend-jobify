99bd29066dfd2640a7a4de022479fa4539166281
Ahmad Nawfal <ahmadnawfal2003@gmail.com>
2023-12-24 01:34:52 +0700

    fix: config

--- server.js
+++ server.js
@@ -40,7 +40,9 @@ cloudinary.config({
 });
 
 // Routers
-
+app.get('/', (req, res) => {
+  res.status(200).json({ msg: 'hello world' });
+});
 app.use('/api/v1/jobs', authenticateUser, jobRouter);
 app.use('/api/v1/users', authenticateUser, userRouter);
 app.use('/api/v1/auth', authRouter);
